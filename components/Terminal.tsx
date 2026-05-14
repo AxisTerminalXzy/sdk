98fe1430ba18879ac663801aba6517f19689d7da
AXIS <kurumamkino@gmail.com>
2026-05-14 14:06:53 +0700

    Create Terminal.tsx

---
+++ components/Terminal.tsx
@@ -0,0 +1,16 @@
+export default function Terminal() {
+  return (
+    <div
+      style={{
+        background: "#111827",
+        padding: "20px",
+        borderRadius: "12px",
+        marginTop: "20px"
+      }}
+    >
+      <p>> /scan trending</p>
+      <p>> /monitor wallets</p>
+      <p>> /execute strategy</p>
+    </div>
+  );
+}
